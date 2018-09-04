1d46c94f7dbd2826bda91c802caef0c3ce07224d
Diego <ldnm12@gmail.com>
2018-09-04 12:00:45 -0600

    Crea las tabs que se van a utilizar en el layout de los restaurantes

---
+++ GastroTEC/app/src/main/java/com/gastrotec/gastrotec/VotoPlatilloTab.java
@@ -0,0 +1,33 @@
+package com.gastrotec.gastrotec;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gastrotec.gastrotec.R;
+
+public class VotoPlatilloTab extends Fragment {
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.tab3, container, false);
+
+    }
+
+}
